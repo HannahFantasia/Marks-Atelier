a7e75afbce6e1c73e48db416033f004eb7a5d690
Astr01d <32207106+Astr01d@users.noreply.github.com>
2021-04-24 22:07:10 +0200

    add image viewer

---
+++ scripts/image_rect.gd
@@ -0,0 +1,15 @@
+extends TextureRect
+
+var image_margins = Vector2(32,32)
+
+
+func _ready():
+	pass
+	
+	
+func _process(delta):
+#	fit the texture to the screen
+#	the image settings maintain aspect ratio and center  
+	rect_size = OS.window_size - image_margins 
+	rect_position = image_margins / 2
+	pass
