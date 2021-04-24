751d8d5a010d7fc1c1ce5995462a830e43c5182c
Hannah Umit <49023943+Koterminion@users.noreply.github.com>
2021-04-24 23:57:46 +0200

    rule of thirds or some shit

--- scripts/image_rect.gd
+++ scripts/image_rect.gd
@@ -6,10 +6,20 @@ var image_margins = Vector2(32,32)
 func _ready():
 	pass
 	
-	
 func _process(delta):
 #	fit the texture to the screen
 #	the image settings maintain aspect ratio and center  
 	rect_size = OS.window_size - image_margins 
 	rect_position = image_margins / 2
 	pass
+
+func _draw():
+	var begin = Vector2(0,0)
+	var end = rect_size
+	var grid_length = 3
+	var grid_width = 3
+	
+	draw_line((Vector2(rect_size.x / 3,0)), (Vector2(rect_size.x / 3, rect_size.y)), Color(255,255,255))
+	draw_line((Vector2(rect_size.x / 3 * 2,0)), (Vector2(rect_size.x / 3 * 2, rect_size.y)), Color(255,255,255))
+	draw_line((Vector2(0, rect_size.y / 3)), (Vector2(rect_size.x, rect_size.y / 3)), Color(255,255,255))
+	draw_line((Vector2(0, rect_size.y / 3 * 2)), (Vector2(rect_size.x, rect_size.y / 3 * 2)), Color(255,255,255))
--- scripts/main.gd
+++ scripts/main.gd
@@ -9,3 +9,4 @@ func _ready():
 	
 func _process(delta):
 	pass
+
