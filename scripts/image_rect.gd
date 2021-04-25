900f50c2ba0a3d823eda94ffdd18a5b29169498f
Hannah Umit <49023943+Koterminion@users.noreply.github.com>
2021-04-25 18:10:45 +0200

    Container and some new stuff
    moved image_rect code to ar_container script. image_rect should draw the grid later on.

--- scripts/image_rect.gd
+++ scripts/image_rect.gd
@@ -1,17 +1,6 @@
 extends TextureRect
 
-var image_margins = Vector2(32,32)
-
-
-func _ready():
-	pass
-	
-func _process(delta):
-#	fit the texture to the screen
-#	the image settings maintain aspect ratio and center  
-	rect_size = OS.window_size - image_margins 
-	rect_position = image_margins / 2
-	pass
-
 func _draw():
-	pass
+	var end = get_texture().get_size()
+	var begin = Vector2(0,0)
+	draw_line(begin, end, Color(255,255,255))
