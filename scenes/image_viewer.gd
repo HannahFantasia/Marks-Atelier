30f4627d7e6ae6d080af3a0741048eb749cea9ee
Hannah Umit <49023943+Koterminion@users.noreply.github.com>
2021-04-26 04:06:31 +0200

    Flipping images now possible
    flip_h, flip_v, greyscale are all intergrated now

--- scenes/image_viewer.gd
+++ scenes/image_viewer.gd
@@ -6,3 +6,17 @@ func _draw():
 	var end = get_size()
 	draw_line(begin, end, ProgramSettings.get_grid_line_colour())
 	
+
+
+func _on_FlipV_pressed():
+	flip_v = not flip_v
+
+
+func _on_FlipH_pressed():
+	flip_h = not flip_h
+
+
+func _on_Greyscale_pressed():
+	var value = material.get_shader_param("is_greyscale")
+	value = not value
+	material.set_shader_param("is_greyscale", value)
